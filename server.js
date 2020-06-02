a8e9ede583e2c2e48c7f4c7e841af970193caa16
joerilapin <joerilapin>
2020-06-02 16:41:14 +0200

    server toegevoegd

---
+++ server.js
@@ -0,0 +1,8 @@
+var http = require('http');
+var server = http.createServer(function (request, response){
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.write('Hello World');
+    response.end();
+});
+server.listen(3000);// start webserver
+console.log('Server gestart op http://localhost:3000...');
